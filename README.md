32b2fac4ae866296621759b274f223db63a7c07b
Hriday Keswani <hridaykeswani@gmail.com>
2022-08-19 12:46:12 +0530

    project init

---
+++ README.md
@@ -0,0 +1,2 @@
+# api_caller
+A cross platform mobile application that can be used for testing APIs from phone
---
+++ android/app/src/main/kotlin/com/hriday/apicaller/api_caller/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hriday.apicaller.api_caller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
